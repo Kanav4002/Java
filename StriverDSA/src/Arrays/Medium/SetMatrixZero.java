03842cec8a663ccb368c12da3c2b73c4d7096aba
Kanav4002 <kumarkanav5753@gmail.com>
2025-08-23 00:35:32 +0530

    Leetcode 73. Set Matrix Zero

---
+++ StriverDSA/src/Arrays/Medium/SetMatrixZero.java
@@ -0,0 +1,114 @@
+package Arrays.Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SetMatrixZero {
+//    public static void main(String[] args) {
+//        int row = 4;
+//        int col = 4;
+//        int[][] arr = {
+//                {1, 1, 1, 1},
+//                {1, 0, 0, 1},
+//                {1, 1, 0, 1},
+//                {1, 1, 1, 1}
+//        };
+//        int[][] result = setZeroes(arr, row, col);
+//
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < col; j++) {
+//                System.out.print(result[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//    static int[][] setZeroes(int[][] nums, int n, int m) {
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                if (nums[i][j] == 0) {
+//                    markRow(nums, i, n, m);
+//                    markCol(nums, j, n, m);
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                if (nums[i][j] == -1) {
+//                    nums[i][j] = 0;
+//                }
+//            }
+//        }
+//
+//        return nums;
+//    }
+//
+//    static void markRow(int[][] nums, int i, int n, int m) {
+//        for (int j = 0; j < m; j++) {
+//            if (nums[i][j] != 0) {
+//                nums[i][j] = -1;
+//            }
+//        }
+//    }
+//
+//    static void markCol(int[][] nums, int j, int n, int m) {
+//        for (int i = 0; i < n; i++) {
+//            if (nums[i][j] != 0) {
+//                nums[i][j] = -1;
+//            }
+//        }
+//    }
+
+
+    /*
+        Better Approach
+        Time Complexity: O(2*(N*M)), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+        Reason: We are traversing the entire matrix 2 times and each traversal is taking O(N*M) time complexity.
+
+        Space Complexity: O(N) + O(M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+        Reason: O(N) is for using the row array and O(M) is for using the col array.
+     */
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 0, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 0, 1)));
+
+        int n = matrix.size();
+        int m = matrix.get(0).size();
+
+        ArrayList<ArrayList<Integer>> result = setZeroes(matrix, n, m);
+
+        for (ArrayList<Integer> row : result) {
+            for (Integer ele  : row) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static ArrayList<ArrayList<Integer>> setZeroes(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+        int[] row = new int[n];
+        int[] col = new int[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix.get(i).set(j, 0);
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
